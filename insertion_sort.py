ccfcbbe462caab473b540493e7cdb6d0dc7f27db
Gborgman05 <gborgman@calpoly.edu>
2020-03-23 17:09:11 -0700

    first

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+#Galen Borgman
+#3/23/2020
+#Insertion Sort
+def main():
+    unsorted = [0, 2, 3, 1, 6, -19, 100, 3]
+    sorted_list = insertion_sort(unsorted)
+    print("unsorted:")
+    print(unsorted)
+    print("sorted:")
+    print(sorted_list)
+if "__name__" == "__main__":
+    main()
