c7373fe83c4c44fa58cbbdac8f1be3a74f5079d5
Gborgman05 <gborgman@calpoly.edu>
2020-10-20 21:00:25 -0700

    max heap stubbing

--- max_heap.py
+++ max_heap.py
@@ -1,3 +1,41 @@
-#Galen Borgman
-#10/20/2020
-#Max Heap
+# Galen Borgman
+# 10/20/2020
+# Max Heap
+
+class MaxHeap:
+    def __init__(self, heap=[]):
+        self.heap = heap
+        self.build_heap()
+
+    def build_heap(self):
+        """ Bottom up heap construction"""
+        pass
+
+    def percolate_up(self, index):
+        pass
+
+    def percolate_down(self, index):
+        pass
+
+    def insert(self, key):
+        pass
+
+    def find_max(self):
+        return self.heap[0]
+
+    def del_max(self):
+        highest = self.heap[0]
+        #TODO delete highest and reorder heap property.
+        return highest
+
+
+def heap_sort(array):
+    heap = MaxHeap(array)
+    answer = []
+    for i in range(len(array)):
+        answer.append(heap.del_max())
+    return answer
+
+
+if __name__ == "__main__":
+    pass
