3af4dcd2597dacd033580bb326443d0c3f34bcab
Gborgman05 <gborgman@calpoly.edu>
2022-07-01 08:29:03 -0700

    daily

--- llist/detect_cycle_2.py
+++ llist/detect_cycle_2.py
@@ -1,29 +1,42 @@
 from llist import ListNode, make_list
 
-class Solution:
-    def detectCycle(self, head):
-        ptr1 = head
-        index = 0
-        while ptr1:
-            ptr2 = ptr1
-            ptr2 = ptr2.next
-            seen = {}
-            while ptr2:
-                if ptr1 == ptr2:
-                    return ptr1
-                if ptr2 in seen:
-                    break
-                else:
-                    seen[ptr2] = "hi"
-                    ptr2 = ptr2.next
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detCycle(self, ptr, order):
+        print([node.val for node in order])
+        cycle_nodes = []
+        saved = ptr
+        while ptr not in cycle_nodes:
+            cycle_nodes.append(ptr)
+            ptr = ptr.next
+        final_nodes = [node for node in order if node in cycle_nodes]
+        if final_nodes:
+            return final_nodes[0]
+        else:
+            return None
+        
+
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        order = []
+        ptr1 = head
+        ptr2 = head
+        while ptr1 and ptr2:
+            order.append(ptr2)
             ptr1 = ptr1.next
-            index += 1
+            ptr2 = ptr2.next
+            order.append(ptr2)
+
+            if not ptr2 or not ptr1:
+                return None
+            else:
+                order.append(ptr2)
+                ptr2 = ptr2.next
+            if ptr1 == ptr2:
+                return self.detCycle(ptr1, order)
         return None
-driver = Solution()
-a = make_list([3,2,0,-4])
-b = a
-while b.next:
-    b = b.next
-b.next = a.next
-print(driver.detectCycle(a))
