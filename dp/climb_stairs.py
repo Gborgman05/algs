77db6762258e95f3338da4493ca1bb28d30db905
Galen Borgman <gborgman@calpoly.edu>
2022-07-27 08:18:26 -0700

    daily

---
+++ dp/climb_stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        store = {}
+        def helper(n):
+            if n < 2:
+                return 1
+            elif n in store:
+                return store[n]
+            else:
+                new = helper(n-1) + helper(n-2)
+                store[n] = new
+                return new
+        return helper(n)
+        
