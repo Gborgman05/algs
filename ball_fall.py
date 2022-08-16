02e099d7693c772132ccd518e74b2d1f945566f6
Galen Borgman <gborgman@calpoly.edu>
2022-08-16 07:18:59 -0700

    daily

--- ball_fall.py
+++ ball_fall.py
@@ -1,68 +1,64 @@
+# initial solution commented out below, didn't work
 class Solution:
     def findBall(self, grid: List[List[int]]) -> List[int]:
-        balls = []
-        for i in range(len(grid[0])):
-            x = i
+        width = len(grid[0])
+        height = len(grid)
+        def go_left(x, y):
+            if x < 1:
+                return False
+            return grid[y][x - 1] == -1 == grid[y][x]
+        def go_right(x, y):
+            if x >= width - 1:
+                return False
+            # print(y, " ", x)
+            # print(len(grid[y]))
+            return grid[y][x + 1] == 1 == grid[y][x]
+        def dfs(x):
             y = 0
-            above = True
-            while True:
-                if above:
-                    if grid[y][x] == 1 and x < len(grid[y]) - 1:
-                        if grid[y][x + 1] == -1:
-                            balls.append(-1)
-                            break
-                        else:
-                            x += 1
-                            above = False
-                    elif grid[y][x] == -1 and x > 0:
-                        if grid[y][x - 1] == 1:
-                            balls.append(-1)
-                            break
-                        else:
-                            x -= 1
-                            above = False
-                    else:
-                        balls.append(-1)
-                        break
-                else:
+            while y < height:
+                if go_left(x, y):
+                    x -= 1
                     y += 1
-                    if y == len(grid) - 1:
-                        balls.append(x - 1)
-                        break
-                    else:
-                        above = True
-        return balls
-class Solution:
-    def findBall(self, grid: List[List[int]]) -> List[int]:
-        balls = []
-        for i in range(len(grid[0])):
-            x = i
-            y = 0
-            above = True
-            while True:
-                if above:
-                    if grid[y][x] == 1 and x < len(grid[y]) - 1:
-                        if if grid[y][x + 1] == -1:
-                            balls.append(-1)
-                            break
-                        else:
-                            x += 1
-                            above = False
-                    elif grid[y][x] == -1 and x > 0:
-                        if grid[y][x - 1] == 1:
-                            balls.append(-1)
-                            break
-                        else:
-                            x -= 1
-                            above = False
-                    else:
-                        balls.append(-1)
-                            break
-                else:
+                elif go_right(x, y):
+                    x += 1
                     y += 1
-                    if y == len(grid) - 1:
-                        balls.append(1):
-                        break
-                    else:
-                        above = True
-        return balls
+                else:
+                    return -1
+            return x
+        res = []
+        for i in range(width):
+            res.append(dfs(i))
+        return res
+                
+        # balls = []
+        # for i in range(len(grid[0])):
+        #     x = i
+        #     y = 0
+        #     above = True
+        #     while True:
+        #         if above:
+        #             if grid[y][x] == 1 and x < len(grid[y]) - 1:
+        #                 if grid[y][x + 1] == -1:
+        #                     balls.append(-1)
+        #                     break
+        #                 else:
+        #                     x += 1
+        #                     above = False
+        #             elif grid[y][x] == -1 and x > 0:
+        #                 if grid[y][x - 1] == 1:
+        #                     balls.append(-1)
+        #                     break
+        #                 else:
+        #                     x -= 1
+        #                     above = False
+        #             else:
+        #                 balls.append(-1)
+        #                 break
+        #         else:
+        #             y += 1
+        #             if y == len(grid) - 1:
+        #                 balls.append(x - 1)
+        #                 break
+        #             else:
+        #                 above = True
+        # return balls
