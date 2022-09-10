491cab2840af38aadbc3e0e253ebafbb78470c47
Galen Borgman <gborgman@calpoly.edu>
2022-09-09 17:27:36 -0700

    daily

---
+++ cut_the_sticks.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cutTheSticks' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def cutTheSticks(arr):
+    # Write your code here
+    store = []
+    while arr:
+        store.append(len(arr))
+        minx = min(arr)
+        arr = [x - minx for x in arr if x - minx > 0]
+    return store
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
