998e4e30ee4adeae38ddf4d85540ed5bed8c4b5e
Galen Borgman <gborgman@calpoly.edu>
2022-09-17 10:01:25 -0700

    daily

--- ds_scratch/bst_test.py
+++ ds_scratch/bst_test.py
@@ -1,1 +1,11 @@
 # tests for bsts
+import unittest
+from bst import Bst
+
+class TestLlist(unittest.TestCase):
+
+    def test_create(self):
+        a = Bst()
+        self.assertEqual(a.nodes, None)
+        b = Bst([1, 2, 3])
+        self.assertEqual(b.nodes.val, 2)
