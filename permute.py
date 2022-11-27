90407b633f4f8c8a1ed2d79d7e16f71bb04dde0c
Galen Borgman <gborgman@calpoly.edu>
2022-11-27 09:21:17 -0800

    daily

--- permute.py
+++ permute.py
@@ -1,20 +1,16 @@
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 1:
-            return [nums]
-        res = self.permute(nums[1:])
-        print(res)
-        cur = nums[0]
-        my_res = []
-        for permute in res:
-            for i in range(len(permute)):
-                my_res.append(permute)
-                my_res[-1].insert(i, cur)
-            my_res.append(permute)
-            my_res[-1].append(cur)
-        return my_res
-            
-            
-        
-        
-        
+    def permute(self, num: List[int]) -> List[List[int]]:
+        n = len(num)
+        ans = []
+
+        def solve(nums, j):
+            if j == n:
+                ans.append(nums[:])
+
+            for i in range(j, n):
+                nums[i], nums[j] = nums[j], nums[i]
+                solve(nums, j+1)
+                nums[i], nums[j] = nums[j], nums[i]
+
+        solve(num, 0)
+        return ans
