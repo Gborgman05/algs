afd0829d99f6329e6c84c256fddfe09216da2274
Gborgman05 <gborgman@calpoly.edu>
2022-12-30 09:27:10 -0800

    used a table - daily

--- js/isomorphicStrings.js
+++ js/isomorphicStrings.js
@@ -4,39 +4,63 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    var convert = function(st, start, char) {
-        n = ""
-        for(let j = 0; j<st.length; j++) {
-            if(st[j] == start) n += char;
-            else {
-                n += st[j]
-            }
-        }
-        console.log(start, char)
-        console.log(n)
-        return n;
-    }
-    if(s.length != t.length) return false;
-    seen = [];
-    for (let i = 0; i<t.length;i++) {
-        if (s[i] != t[i]) {
-            
-            if (!(s[i] in seen || t[i] in seen)){
-                console.log(seen)
-                console.log(s[i], t[i])
-                seen.push(s[i])
-                seen.push(t[i])
-                s = convert(s, s[i], t[i])
-            }
-            else {
-                return false
+    if (s.length != t.length) return false;
+    let mapped = []
+    let person = {};
+    for (let i = 0;i<s.length;i++) {
+        if(s[i] in person) {
+            // console.log(s[i], t[i])
+            if(person[s[i]] != t[i]) {
+                return false;
             }
         }
         else {
-            seen.push(s[i])
-            // seen.push(t[i])
-        } 
+            if(mapped.includes(t[i])) {
+                return false;
+            }
+            // console.log(mapped)
+            // console.log(t[i])
+            
+            person[s[i]] = t[i];
+            mapped.push(t[i])
+        }
     }
-    // console.log(s)
-    return s == t
+    // console.log(person)
+    return true;
+    
+    // var convert = function(st, start, char) {
+    //     n = ""
+    //     for(let j = 0; j<st.length; j++) {
+    //         if(st[j] == start) n += char;
+    //         else {
+    //             n += st[j]
+    //         }
+    //     }
+    //     console.log(start, char)
+    //     console.log(n)
+    //     return n;
+    // }
+    // if(s.length != t.length) return false;
+    // seen = [];
+    // for (let i = 0; i<t.length;i++) {
+    //     if (s[i] != t[i]) {
+            
+    //         if (!(s[i] in seen || t[i] in seen)){
+    //             console.log(seen)
+    //             console.log(s[i], t[i])
+    //             seen.push(s[i])
+    //             seen.push(t[i])
+    //             s = convert(s, s[i], t[i])
+    //         }
+    //         else {
+    //             return false
+    //         }
+    //     }
+    //     else {
+    //         seen.push(s[i])
+    //         // seen.push(t[i])
+    //     } 
+    // }
+    // // console.log(s)
+    // return s == t
 };
