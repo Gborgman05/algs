66e0bd371cccaa84e5b14e112ca54bdc9551fc13
Gborgman05 <gborgman@calpoly.edu>
2023-02-09 15:13:26 -0800

    daily

---
+++ removeElement.ts
@@ -0,0 +1,8 @@
+function removeElement(nums: number[], val: number): number {
+    let i : number = 0;
+    while(i < nums.length) {
+        if(nums[i] == val) nums.splice(i, 1);
+        else i++;
+    }
+    return 0
+}; 
