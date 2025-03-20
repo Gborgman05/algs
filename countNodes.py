8b33bb708f5c17cc78ad32fce0772057ac8a248f
gborgman <Gborgman@calpoly.edu>
2025-03-20 15:15:33 -0700

    3/20

--- countNodes.py
+++ countNodes.py
@@ -8,17 +8,20 @@ class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
         cnt = 0
         levels = 0
+        to_left = 0
         while True:
+            to_left *= 2
             if not root:
                 return 0
             if root.left and root.right:
                 levels += 1
                 root = root.right
+                to_left += 1
             elif root.left:
-                return self.calcCount(levels, isRight)
+                return self.calcCount(levels) + 1 + 2 * to_left
             elif not root.left and not root.right:
                 return self.calcCount(levels)
-    def calcCount(self, levels, isRight):
+    def calcCount(self, levels):
         sm = 0
         for i in range(levels + 1):
             sm += 2 ** i
