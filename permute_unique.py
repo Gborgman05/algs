3e6f53e82a955cd291c5e1a838641bd9f8e8367a
gborgman <Gborgman@calpoly.edu>
2025-04-04 15:08:49 -0700

    fixed backtracking problem

--- permute_unique.py
+++ permute_unique.py
@@ -7,17 +7,18 @@ class Solution:
             else:
                 counts[a] = 1
         sol = []
-        for char in counts:
-            sol.append(char)
-            if counts[char] <= 1:
-                del counts[char]
+        def helper(curr, counts):
+            if len(curr) == len(nums):
+                sol.append(list(curr))
+                return []
             else:
-                counts[char] -= 1
-    def helper(self, curr):
-        next_level = []
-        for i in range(len(curr)):
-            poss = curr[0]
-            for char in poss:
-                new_poss = char
-                next_level.append([curr[i][1] + [char])
-        return next_level
+                for key in counts:
+                    if counts[key] > 0:
+                        counts[key] -= 1
+                        curr.append(key)
+                        helper(curr, counts)
+                        curr.pop()
+                        counts[key] += 1
+        helper([], counts)
+        return sol
+        
