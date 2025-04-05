1ebbd070772d82c94f480f1596ac2c394b5c87b9
gborgman <Gborgman@calpoly.edu>
2025-04-05 13:22:43 -0700

    4/5

---
+++ is_anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_store = {}
+        for char in s:
+            if char in s_store:
+                s_store[char] += 1
+            else:
+                s_store[char] = 1
+        for char in t:
+            if char in s_store:
+                if s_store[char] <= 0:
+                    return False
+                s_store[char] -= 1
+            else:
+                return False
+        for key in s_store:
+            if s_store[key] != 0:
+                return False
+        return True
