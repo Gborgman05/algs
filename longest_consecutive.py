315beb8f20dc465e4c0a8f5e0e13315483d05999
gborgman <Gborgman@calpoly.edu>
2025-04-09 14:43:18 -0700

    longest_consecutive

--- longest_consecutive.py
+++ longest_consecutive.py
@@ -1,5 +1,7 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
         store = {}
         for num in nums:
             store[num] = 1
@@ -11,14 +13,14 @@ class Solution:
                     already_seen[back] = already_seen[back-1] + 1
                     break
                 else:
-                    already_seen[back] = 1
                     back -= 1
-                
+                    already_seen[back] = 1
+            print(forward, " ", num, " ", back)
+            forward = back
+            if num not in already_seen:
+                already_seen[num] = 1
             while forward+1 in store:
-                if forward+1 in already_seen:
-                    already_seen[]
-
-            else:
-                store[num] = 1
-        return max(store.values())
+                already_seen[forward+1] = already_seen[forward] + 1
+                forward += 1
+        return max(already_seen.values())
         
