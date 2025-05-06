8a013466d033f9ccd65145591ed3a26139501db4
Galen Borgman <gbrgman365@gmail.com>
2025-05-06 08:35:36 -0700

    5/6

---
+++ longest_substring.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        store = {}
+        cnt = 0
+        max_count = 0
+        l = 0
+        for r in range(len(s)):
+            while r >= l and s[r] in store:
+                del store[s[l]]
+                l += 1
+            store[s[r]] = 1
+            max_count = max(max_count, r - l  + 1)
+        return max_count
+
+
+
+
