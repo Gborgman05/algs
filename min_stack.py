9c6dad06d68a1213c733fa629269d6d2f5884587
Gborgman05 <gborgman@calpoly.edu>
2025-06-10 08:36:53 -0700

    6/10

--- min_stack.py
+++ min_stack.py
@@ -31,3 +31,40 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.mins = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.stack) < 2:
+            self.mins.append(val)
+        else:
+            self.mins.append(min(self.mins[-1], val))
+        
+        
+
+    def pop(self) -> None:
+        self.mins.pop()
+        return self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.mins[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
--- product_except_self.py
+++ product_except_self.py
@@ -14,3 +14,17 @@ class Solution:
             else:
                 suffixes[i] = suffixes[i+1] * nums[i+1]
         return [prefixes[i] * suffixes[i] for i in range(len(nums))]
+    
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        before = [1] * len(nums)
+        for i in range(len(nums)):
+            if i == 0:
+                continue
+            else:
+                before[i] = before[i-1] * nums[i-1]
+
+        suffix = [1] * len(nums)
+        for i in range(len(nums) - 2, -1, -1):
+            suffix[i] = suffix[i+1] *nums[i+1]
+        return [before[i] * suffix[i] for i in range(len(nums))]
