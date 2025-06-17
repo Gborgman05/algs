487786c108f3f37adc343b392ad26aab19a0b5ae
Gborgman05 <gborgman@calpoly.edu>
2025-06-17 11:44:19 -0700

    6/17

--- climb_stairs.py
+++ climb_stairs.py
@@ -4,3 +4,17 @@ class Solution:
         while len(items) < n:
             items.append(items[-1] + items[-2])
         return items[n -1]
+    
+
+    class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        stairs = [0] * n
+        stairs[0] = 1
+        stairs[1] = 2
+        for i in range(2, n):
+            stairs[i] = stairs[i-1] + stairs[i-2]
+        return stairs[-1]
+
+        
--- min_cost_climbing_stairs.py
+++ min_cost_climbing_stairs.py
@@ -5,3 +5,12 @@ class Solution:
             cumulative[i] = min(cost[i-1] + cumulative[i-1], cost[i-2] + cumulative[i-2])
         #print(cumulative)
         return cumulative[-1] 
+    
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        min_costs = [0] * (len(cost) + 1)
+        #min_costs[1] = cost[0]
+        for i in range(2, len(cost) + 1):
+            min_costs[i] = min(min_costs[i-1] + cost[i-1], min_costs[i-2] + cost[i-2])
+        return min_costs[-1]
+        
