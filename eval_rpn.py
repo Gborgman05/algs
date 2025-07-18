8021394e6bbc6cb139963049ef69768e01535575
Gborgman05 <gborgman@calpoly.edu>
2025-07-18 13:33:59 -0700

    7/18

--- eval_rpn.py
+++ eval_rpn.py
@@ -30,4 +30,27 @@ class Solution:
         return stack[0]
 
 
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token in ('+', '-', '*', '/'):
+                op2 = stack.pop()
+                op1 = stack.pop()
+                res = None
+                if token == '+':
+                    res = op1 + op2
+                elif token == '-':
+                    res = op1 - op2
+                elif token == '*':
+                    res = op1 * op2
+                elif token == '/':
+                    res = math.trunc(op1 / op2)
+                stack.append(res)
+            else:
+                stack.append(int(token))
+            # print(stack)
+        return stack[0]
+                    
         
