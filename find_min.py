bb3ee3a66daf4fc8eb6c2d5e5ae2af81bb58a6e9
gborgman <Gborgman@calpoly.edu>
2025-08-04 08:47:48 -0700

    8/4

--- find_min.py
+++ find_min.py
@@ -17,3 +17,13 @@ class Solution:
         if nums[0] < nums[r]:
             return nums[0]
         return nums[r]
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        return nums[l]
