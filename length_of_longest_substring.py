192583ce46193d45b831f6c6505cdf6b905f7f68
gborgman <Gborgman@calpoly.edu>
2025-08-06 16:48:10 -0700

    8/6

--- length_of_longest_substring.py
+++ length_of_longest_substring.py
@@ -24,3 +24,24 @@ class Solution:
                 if curr <= len(s):
                     max_len = max(max_len, curr - start)
         return max_len
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+        store = {}
+        l = 0
+        r = 0
+        max_len = 1
+        store[s[0]] = 1
+        while r + 1 < len(s):
+            while s[r+1] in store:
+                store[s[l]] -= 1
+                if store[s[l]] == 0:
+                    del store[s[l]]
+                l += 1
+            store[s[r+1]] = 1
+            r += 1
+            max_len = max(r - l + 1, max_len)
+        return max_len
+
