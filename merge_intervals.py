905fa88c248ce621b8d89691d83741499b8a2820
gborgman <Gborgman@calpoly.edu>
2025-08-12 07:32:13 -0700

    8/12

--- merge_intervals.py
+++ merge_intervals.py
@@ -33,4 +33,15 @@ class Solution:
         #     final.append(curr_interval)
         # return final
 
+    class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # idk if its sorted or not
+        intervals = sorted(intervals, key=lambda a: a[0])
+        output = [intervals[0]]
+        for start, end in intervals:
+            if start <= output[-1][1]:
+                output[-1][1] = max(end, output[-1][1])
+            else:
+                output.append([start, end])
+        return output
         
