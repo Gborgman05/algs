f0f113256bc103cdf4d99fdfadab82870d0801a8
gborgman <Gborgman@calpoly.edu>
2025-08-28 10:56:16 -0700

    8/28

--- reorder_list.py
+++ reorder_list.py
@@ -46,4 +46,31 @@ class Solution:
             head = head.next.next
 
 
-        
+    # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow, fast = head, head
+        while fast and fast.next:
+            if fast.next.next:
+                slow = slow.next
+                fast = fast.next.next
+            else:
+                fast = fast.next.next
+        print(slow)
+        # cut and reverse
+        later = slow.next
+        slow.next = None
+        prev = None
+        while later:
+            tmp = later.next
+            .next = curr
+            later = later.next
+            curr = tmp
+        print(curr)
