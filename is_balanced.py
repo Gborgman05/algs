be0057d3808152a0b962736aeb8d2ae85d62df48
Gborgman05 <gborgman@calpoly.edu>
2025-11-07 08:57:51 -0800

    11/7

--- is_balanced.py
+++ is_balanced.py
@@ -49,4 +49,19 @@ class Solution:
                 return (l[0] and r[0] and 1 >=abs(l[1] - r[1]), 1 + max(l[1], r[1]))
         
         return isBalancedAndHeight(root)[0]
-        
+        # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if not node:
+                return (True, 0)
+            else:
+                l = dfs(node.left)
+                r = dfs(node.right)
+                return (l[0] and r[0] and abs(l[1] - r[1]) <= 1, 1 + max(l[1], r[1]))
+        return dfs(root)[0]
