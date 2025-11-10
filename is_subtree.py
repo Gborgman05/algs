09b3f8e335bcd9e632efcf890a9de88f557e7aa1
Gborgman05 <gborgman@calpoly.edu>
2025-11-10 09:26:41 -0800

    11/10

--- is_subtree.py
+++ is_subtree.py
@@ -25,4 +25,26 @@ class Solution:
 
 
 
-        
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def eq(node, sub):
+            if not node and not sub:
+                return True
+            elif not node or not sub:
+                return False
+            else:
+                return node.val == sub.val and eq(node.left, sub.left) and eq(node.right, sub.right)
+        def dfs(node):
+            if not node:
+                return not subRoot
+            if node and node.val == subRoot.val:
+                if eq(node, subRoot):
+                    return True
+            return dfs(node.left) or dfs(node.right)
+        return dfs(root)
