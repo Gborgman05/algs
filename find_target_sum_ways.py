0176f8180f928683af8e600b6c5ecf10c3663f27
Gborgman05 <gborgman@calpoly.edu>
2026-01-26 07:48:58 -0800

    1/26

--- find_target_sum_ways.py
+++ find_target_sum_ways.py
@@ -17,3 +17,21 @@ class Solution:
         if target not in store:
             return 0
         return store[target]
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        """
+        lets try naive first where we try every possible + - for each 
+        this is like O(2^n) where n is the length of nums
+        this is because you double the size of the can_make array
+        for each new number you add. 
+        """
+        can_make = [0]
+        for num in nums:
+            new_can_make = []
+            for make_num in can_make:
+                new_can_make.append(make_num-num)
+                new_can_make.append(make_num+num)
+            can_make = new_can_make
+        return len([1 for num in can_make if num==target])
+        
