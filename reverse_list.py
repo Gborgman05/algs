8ae6377264a60c7745ded7391a0c3b28a2e87ae6
Gborgman05 <gborgman@calpoly.edu>
2026-02-03 15:47:28 -0800

    2/3

--- reverse_list.py
+++ reverse_list.py
@@ -7,3 +7,18 @@ class Solution:
             node = head
             head = nxt
         return node
+
+        # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        nxt = None
+        while head:
+            tmp = head.next
+            head.next = nxt
+            nxt = head
+            head = tmp
+        return nxt
