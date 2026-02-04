56ff5c294d4cff8a55cffa9f89dd063722d973af
Gborgman05 <gborgman@calpoly.edu>
2026-02-04 07:54:32 -0800

    2/4

--- merge_two_lists.py
+++ merge_two_lists.py
@@ -47,3 +47,35 @@ class Solution:
         return saved
         
 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
+        saved = head
+        while list1 and list2:
+            if list1.val < list2.val:
+                tmp = list1
+                list1 = list1.next
+                head.next = tmp
+                head = head.next
+            else:
+                tmp = list2
+                list2 = list2.next
+                head.next = tmp
+                head = head.next
+        if not head:
+            if not list2:
+                return list1
+            if not list1:
+                return list2
+        else:
+            if list1:
+                head.next = list1
+            elif list2:
+                head.next = list2
+        return saved.next
