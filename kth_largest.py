1a7a5d5b22ccb2193d5714a966a208616bbcd350
Gborgman05 <gborgman@calpoly.edu>
2026-02-10 09:24:45 -0800

    2/10

--- kth_largest.py
+++ kth_largest.py
@@ -19,3 +19,26 @@ class KthLargest:
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = nums
+        heapq.heapify(nums)
+        self.amount = k
+        while len(self.heap) > self.amount:
+            heapq.heappop(self.heap)
+        
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.amount:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
