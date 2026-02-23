091733b6c90bd0e09a1dbc3bfb76f05e225ef899
Gborgman05 <gborgman@calpoly.edu>
2026-02-23 13:15:08 -0800

    quest

--- daily_temperatures.py
+++ daily_temperatures.py
@@ -24,3 +24,15 @@ class Solution:
                 stack.pop()
             stack.append(i)
         return ans
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                j = stack.pop()
+                temperatures[j] = i-j
+            stack.append(i)
+        for i in stack: # never found one
+            temperatures[i] = 0
+        return temperatures 
