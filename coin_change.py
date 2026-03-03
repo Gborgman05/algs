232d79491939fce155be9219dd81576be4df4891
Gborgman05 <gborgman@calpoly.edu>
2026-03-03 10:25:52 -0800

    3/3

--- coin_change.py
+++ coin_change.py
@@ -21,3 +21,20 @@ class Solution:
                 num_coins[i] = min(poss)
         # print(num_coins)
         return num_coins[-1]
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        min_coins = [None] * (amount + 1)
+        min_coins[0] = 0
+        for i in range(len(min_coins)):
+            for a in coins:
+                if i - a >= 0 and min_coins[i-a] is not None:
+                    if min_coins[i] is None :
+                        min_coins[i] = min_coins[i-a] + 1
+                    else:
+                        min_coins[i] = min(min_coins[i], min_coins[i-a] + 1)
+        if min_coins[-1] is None:
+            return -1
+        else:
+            return min_coins[-1]
+
